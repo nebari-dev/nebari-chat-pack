8a7f2bb3c412d9ecf87c621b0bcebe7cc4d97c20
Philip Meier <github.pmeier@posteo.de>
2026-01-17 22:42:01 +0100

    fix API URL configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,34 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 
-import dotenv from 'dotenv';
-dotenv.config();
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-const apiUrl = process.env.VITE_API_URL;
-
-export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: 'react',
-      semicolons: true
-    }),
-    tailwindcss(),
-    react()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: apiUrl,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+  return {
+    plugins: [
+      tanstackRouter({
+        target: 'react',
+        semicolons: true
+      }),
+      tailwindcss(),
+      react()
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
-  }
+  };
 });
