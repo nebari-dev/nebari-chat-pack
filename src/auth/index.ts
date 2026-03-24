f2b18bc20cf6118793a896561f9708b2f8f343d9
Chris Colbert <sccolbert@gmail.com>
2026-03-24 15:05:23 -0500

    docs

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -83,7 +83,7 @@ async function login(): Promise<void> {
 /**
  * A function which handles user logout via Keycloack.
  *
- * If the user is already logged-in this just a simple redirect.
+ * If the user is already logged-out this just a redirect to origin.
  */
 export
 async function logout(): Promise<void> {
